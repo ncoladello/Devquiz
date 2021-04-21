facd17176d163b36f25c10bb3fa958ed552b5872
Natanael Coladello <natanael.coladello@fatec.sp.gob.br>
2021-04-21 10:34:20 -0300

    Initial commit

---
+++ bytebank_sqlite/android/app/src/main/kotlin/br/com/natanaelcoladello/bytebank_sqlite/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.natanaelcoladello.bytebank_sqlite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bytebank_sqlite/lib/database/app_database.dart
@@ -0,0 +1,16 @@
+import 'package:bytebank_sqlite/database/dao/contact_dao.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+Future<Database> getDatabase() async {
+  final String path = join(await getDatabasesPath(), 'bytebank.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute(ContactDao.tableSql);
+    },
+    version: 1,
+//      onDowngrade: onDatabaseDowngradeDelete,
+  );
+}
+
